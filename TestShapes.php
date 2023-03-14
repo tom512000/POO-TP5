19bdc721cde0de18647cd8731983d3a5ce8b152f
TOM SIKORA <siko0001@2A4F1-31UPC108.ad-urca.univ-reims.fr>
2023-03-14 15:26:26 +0100

    Fin de séance

--- TestShapes.php
+++ TestShapes.php
@@ -2,3 +2,12 @@
 declare(strict_types=1);
 
 require_once "Shape.php";
+
+# Question 1
+$figure1 = new Shape(6);
+var_dump($figure1);
+# - vertices -> int -> 6
+# Le mode d'accès de l'attribut vertices est protected.
+# public    : Accessible à tout le monde
+# private   : Accessible uniquement à l'intérieur de la classe
+# protected : Accessible uniquement aux classes filles
